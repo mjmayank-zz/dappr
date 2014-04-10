2efeaf8cd092f4bf931db79c74d7e41bd17e33af
Mayank Jain <mjmayank@gmail.com>
2014-04-09 22:24:01 -0500

    Initial Commit

---
+++ Dappr/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Dappr
+//
+//  Created by Mayank Jain on 4/9/14.
+//  Copyright (c) 2014 Mayank Jain. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
