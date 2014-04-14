57d6277a78ff6acbf9908c1e44ad595628c73815
Mayank Jain <mjmayank@gmail.com>
2014-04-14 14:38:12 -0500

    fixed warning

---
+++ Dappr/AddOutfitViewController.h
@@ -0,0 +1,14 @@
+//
+//  AddOutfitViewController.h
+//  Dappr
+//
+//  Created by Mayank Jain on 4/14/14.
+//  Copyright (c) 2014 Mayank Jain. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AddOutfitViewController : UIViewController<UISearchBarDelegate, UISearchDisplayDelegate, UITableViewDelegate>
+@property (strong, nonatomic) IBOutlet UISearchBar *searchBar;
+
+@end
---
+++ Dappr/AddOutfitViewController.m
@@ -0,0 +1,52 @@
+//
+//  AddOutfitViewController.m
+//  Dappr
+//
+//  Created by Mayank Jain on 4/14/14.
+//  Copyright (c) 2014 Mayank Jain. All rights reserved.
+//
+
+#import "AddOutfitViewController.h"
+
+@interface AddOutfitViewController ()
+
+@end
+
+@implementation AddOutfitViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+    self.searchBar.delegate = self;
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
+{
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+
+
+@end
